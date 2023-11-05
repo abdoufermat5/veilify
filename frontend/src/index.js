5cffcf52b337cb86fde5a4b54f0f813fea7efff5
abdoufermat5 <saabdouyaya@ept.sn>
2023-11-05 19:53:48 +0100

    add analytics

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { inject } from '@vercel/analytics';
+import "./index.css";
 import "cropperjs/dist/cropper.css";
-import './index.css';
-import App from './App';
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-reportWebVitals()
+reportWebVitals();
+inject();
